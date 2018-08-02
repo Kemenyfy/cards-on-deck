33c4e67ddcaaf0639915a73bee7cc75424ab38ef
Kemenyfy <gustavo.kemenyfy@gmail.com>
2018-08-01 22:20:02 -0400

    CSS and polished site

--- public/main.js
+++ public/main.js
@@ -1,9 +1,16 @@
-// const deck = () => {
+const suits = ["Hearts", "Spades", "Clubs", "Diamonds"]
 
-// }
-const deck = [1,2,3,4,5,6]
+const rank = ['Ace of ','2 of ','3 of ','4 of ','5 of ','6 of ','7 of ','8 of ','9 of ','10 of ','Jack of ','Queen of ','King of ']
+
+const deck = []
+
 
 shuffleDeck = () => {
+  for (let i = 0; i < rank.length; i++) {
+    for (let j = 0; j < suits.length; j++) {
+      deck.push(rank[i] + suits[j])
+      }
+  }
   // Randomly shuffle cards from array
   let i = 0
   let j = 0
@@ -19,8 +26,9 @@ shuffleDeck = () => {
 }
 
 const dealCard = () => {
-  console.log(deck.pop()); 
-  document.querySelector('.dealtCard').textContent = deck.pop() 
+  const cardDealt = deck.pop()
+  console.log(cardDealt); 
+  document.querySelector('.dealtCard').textContent = cardDealt
 }
 
 document.addEventListener('DOMContentLoaded', shuffleDeck)
